bb1a68676cd209dd748d5be48c9739bdd2442c72
Brandon-Magalhaes <brandonmagalhaes.developer@hotmail.com>
2025-01-13 16:03:45 -0300

    FIX removed metadata error hidratation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,4 @@
-import type { Metadata } from "next";
 import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "Pix On Chain",
-  description: "Landing Page",
-};
 
 export default function RootLayout({
   children,
