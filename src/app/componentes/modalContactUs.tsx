5aa16d3b2f4215bd8d1221ee087625c0247d9919
Pedro Magalhaes <pedrosgmagalhaes@gmail.com>
2025-01-14 15:55:55 -0300

    updated modal

--- src/app/componentes/modalContactUs.tsx
+++ src/app/componentes/modalContactUs.tsx
@@ -5,7 +5,7 @@ import logo from "@/app/assets/pixonchain.png";
 import bg from "@/app/assets/bg-modal.png";
 import vip from "@/app/assets/cartao-vip.png";
 import check from "@/app/assets/check.png";
-import whatsappBlack from "@/app/assets/whatsapp-black.png";
+import { FaWhatsapp } from "react-icons/fa";
 
 interface ModalContactUsProps {
   setIsOpenModal: (isOpen: boolean) => void;
@@ -16,8 +16,14 @@ const ModalContactUs = ({ setIsOpenModal }: ModalContactUsProps) => {
   const whatsappLink = `https://wa.me/${whatsappNumber}`;
 
   return (
-    <div className="absolute inset-0 flex items-center justify-center z-40 overflow-y-auto bg-black bg-opacity-50">
-      <div className="relative w-full max-w-[984px] bg-[#7747FF] text-white rounded-lg shadow-lg z-60 lg:py-7 md:py-6 sm:py-5 py-4 min-h-screen flex flex-col justify-center overflow-y-auto">
+    <div
+      className="absolute inset-0 flex items-center justify-center z-40 overflow-y-auto bg-black bg-opacity-50"
+      onClick={() => setIsOpenModal(false)}
+    >
+      <div
+        className="relative w-full max-w-[984px] bg-[#7747FF] text-white rounded-lg shadow-lg z-50 flex flex-col overflow-y-auto mt-2 lg:mb-2 sm:mt-10"
+        onClick={(e) => e.stopPropagation()}
+      >
         <Image
           src={bg}
           alt="Imagem de fundo"
@@ -27,22 +33,29 @@ const ModalContactUs = ({ setIsOpenModal }: ModalContactUsProps) => {
         />
         <button
           className="absolute top-4 right-4 z-50"
-          onClick={() => setIsOpenModal(false)}
+          onClick={() => setIsOpenModal(false)} 
           aria-label="Fechar Modal"
         >
-          <IoIosCloseCircle className="h-8 w-8" color="white" />
+          <IoIosCloseCircle className="h-8 w-8 text-white" />
         </button>
 
-        <div className="w-full flex items-center justify-center lg:mb-7 md:mb-6 sm:mb-5 mb-4">
-          <Image src={logo} alt="Logo Pix On Chain" width={102} height={45} />
-        </div>
+        <div className="w-full flex flex-col px-5 sm:py-5 lg:px-[127px] md:px-[80px] sm:px-[40px] z-50 relative lg:p-8">
+        <div className="flex flex-col">
+          <div className="w-full flex items-center justify-center mb-6">
+            <Image
+              src={logo}
+              alt="Logo Pix On Chain"
+              width={102}
+              height={45}
+              className="object-contain"
+              priority
+            />
+          </div>
 
-        <div className="w-full flex flex-col lg:px-[117px] md:px-[80px] sm:px-[40px] px-5 z-50 relative">
-          <div className="flex flex-col gap-3 lg:mb-7 md:mb-6 sm:mb-5 mb-4">
-            <h1 className="text-center font-bold lg:text-xl md:text-lg sm:text-base text-sm">
+          <h1 className="text-center font-bold lg:text-xl md:text-lg sm:text-base text-sm">
               Acesso Exclusivo ao Club do Pix on Chain
             </h1>
-            <p className="text-xs lg:text-sm 2xl:text-base font-normal 2xl:leading-6 lg:leading-5 sm:leading-4 leading-4 text-justify">
+            <p className="text-xs lg:text-sm 2xl:text-base font-normal text-justify">
               <span className="text-black font-extrabold">Pix on Chain</span> é
               uma plataforma inovadora que possibilita a{" "}
               <span className="font-bold">
@@ -60,25 +73,24 @@ const ModalContactUs = ({ setIsOpenModal }: ModalContactUsProps) => {
             </p>
           </div>
 
-          <div className="flex flex-col gap-2 lg:mb-7 md:mb-6 sm:mb-5 mb-4">
-            <h1 className="2xl:text-xl lg:text-lg md:text-base text-sm 2xl:leading-8 lg:leading-7 md:leading-6 leading-5 font-bold flex gap-2">
+          <div className="flex flex-col gap-2 mb-6">
+            <h1 className="flex gap-2 font-bold lg:text-lg md:text-base text-sm">
               <Image src={vip} alt="Logo Pix On Chain" width={32} height={22} />
               Garanta seu acesso ao{" "}
               <span className="text-black">Early Adoption Pass</span>
             </h1>
-            <p className="text-xs lg:text-sm font-medium lg:leading-6 md:leading-5 sm:leading-4 leading-4">
+            <p className="text-xs lg:text-sm font-medium">
               Seja um dos pioneiros na transformação do mercado de criptomoedas.
               Ao adquirir o passe, você se tornará parte de um grupo exclusivo
               no universo cripto.
             </p>
           </div>
 
-          <div className="bg-white rounded-xl p-7 flex flex-col gap-3 text-black lg:mb-7 md:mb-6 sm:mb-5 mb-4">
-            <h1 className="font-normal 2xl:leading-8 lg:leading-7 md:leading-6 leading-5 2xl:text-xl lg:text-lg md:text-base text-sm">
+          <div className="bg-white rounded-xl p-7 flex flex-col gap-3 text-black mb-6">
+            <h1 className="font-normal lg:text-lg md:text-base text-sm">
               Com o <span className="font-bold">Pix on Chain,</span> você
               poderá:
             </h1>
-
             {[
               "Gerenciar carteiras digitais de criptomoedas com eficiência.",
               "Realizar transações de forma segura de cripto (Hathor, Bitcoin, Ethereum, USDT, USDC).",
@@ -98,7 +110,7 @@ const ModalContactUs = ({ setIsOpenModal }: ModalContactUsProps) => {
             ))}
           </div>
 
-          <p className="font-normal text-xs 2xl:leading-5 lg:leading-4 leading-4 lg:mb-7 md:mb-6 sm:mb-5 mb-4">
+          <p className="text-xs mb-6">
             DISCLAMER:{" "}
             <span className="text-black font-bold">PIX ON CHAIN</span> É UMA
             PLATAFORMA CONSTRUÍDA COM{" "}
@@ -115,18 +127,12 @@ const ModalContactUs = ({ setIsOpenModal }: ModalContactUsProps) => {
             USUÁRIOS REALIZEM SUAS PRÓPRIAS ANÁLISES ANTES DE SE ENVOLVEREM
             NESSE TIPO DE ATIVIDADE.
           </p>
-
           <a
             href={whatsappLink}
-            className="z-50 relative bg-white rounded-lg gap-2 flex items-center justify-center text-black font-medium 2xl:text-xl lg:text-lg md:text-base text-sm 2xl:leading-8 lg:leading-7 md:leading-6 leading-5 py-3"
+            className="bg-[#25d366] rounded-lg gap-2 flex items-center justify-center text-white font-medium py-3 animate-pulse"
           >
             FALAR COM UM ESPECIALISTA
-            <Image
-              src={whatsappBlack}
-              alt="Icone de Whatsapp"
-              width={32}
-              height={32}
-            />
+            <FaWhatsapp className="text-white w-6 h-6 ml-2" />
           </a>
         </div>
       </div>
