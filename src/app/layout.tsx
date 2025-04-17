77c6dfb52bacd6af7789f13f879d149b1f2a9ce8
Brandon Magalhães <brandonmagalhaes.developer@hotmail.com>
2025-04-17 11:34:51 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,6 @@ import { TenantProvider } from "./context/TenantContext";
 import { TenantUrlProvider } from "./context/TenantUrlContext";
 import { LanguageProvider } from "./utils/languageContext";
 
-export const metadata = {
-  title: "Pix on Chain - A Corretora de Criptomoedas.",
-  description: "Pix on Chain - tornamos fácil aquilo que parecia difícil.",
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
